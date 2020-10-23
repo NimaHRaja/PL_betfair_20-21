6569eff88b35d08ce88eb6c0472c7e4739f187e9
NimaHRaja <nimaradja@gmail.com>
2020-10-23 10:07:40 +0100

    data_folder updated

--- init.R
+++ init.R
@@ -1,6 +1,7 @@
 #### Installing and loading libraries ####
 
 # devtools::install_github("durtal/betfaiR", force = TRUE)
+options(stringsAsFactors = FALSE)
 library(betfaiR)
 library(dplyr)
 
@@ -10,5 +11,14 @@ nima_login_data <-
     read.csv("my_login_data.csv")
 
 my_bf <- betfair(usr = nima_login_data$username, 
-              pwd = nima_login_data$password, 
-              key = as.character(nima_login_data$applicationKey))
+                 pwd = nima_login_data$password, 
+                 key = as.character(nima_login_data$applicationKey))
+
+data_folder <- read.csv("data_folder.csv") %>% filter(Event == "PL20-21") %>% 
+    select(folder) %>% as.vector() %>% as.character()
+
+source("get_and_save_a_market.R")
+
+# PL <- my_bf$marketCatalogue(maxResults = 1000, filter = marketFilter(eventIds = 2022802))
+
+# lapply(PL,get_and_save_a_market, data_folder)
