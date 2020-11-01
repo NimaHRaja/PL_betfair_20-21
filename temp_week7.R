74d4faef30a8dc861a4f16feeeb00261f1974f6e
NimaHRaja <nimaradja@gmail.com>
2020-11-01 20:59:17 +0000

    week7 added

--- temp_week7.R
+++ temp_week7.R
@@ -39,3 +39,152 @@ while (Sys.time() < "2020-10-31 14:20:00 BST"){
 }
 
 ###############################
+
+# my_bf$events(filter = marketFilter(competitionIds = 10932509)) %>% View() # 30072811	
+
+
+while (Sys.time() < "2020-10-31 14:20:00 BST"){
+    
+    SHUMCI <- 
+        my_bf$marketCatalogue(maxResults = 100, filter = marketFilter(eventIds = 30072811))
+    
+    PL <- 
+        my_bf$marketCatalogue(maxResults = 1000, filter = marketFilter(eventIds = 2022802))
+    
+    
+    lapply(SHUMCI,get_and_save_a_market, data_folder)
+    lapply(PL,get_and_save_a_market, data_folder)
+    
+    print(Sys.time())
+    Sys.sleep(time = 100)
+}
+
+###############################
+
+# my_bf$events(filter = marketFilter(competitionIds = 10932509)) %>% View() # 30072802	
+
+
+while (Sys.time() < "2020-10-31 16:20:00 BST"){
+    
+    BURCHE <- 
+        my_bf$marketCatalogue(maxResults = 100, filter = marketFilter(eventIds = 30072802))
+    
+    PL <- 
+        my_bf$marketCatalogue(maxResults = 1000, filter = marketFilter(eventIds = 2022802))
+    
+    
+    lapply(BURCHE,get_and_save_a_market, data_folder)
+    lapply(PL,get_and_save_a_market, data_folder)
+    
+    print(Sys.time())
+    Sys.sleep(time = 300)
+}
+
+###############################
+
+# my_bf$events(filter = marketFilter(competitionIds = 10932509)) %>% View() # 30073969	
+
+
+while (Sys.time() < "2020-10-31 19:20:00 BST"){
+    
+    LIVWHU <- 
+        my_bf$marketCatalogue(maxResults = 100, filter = marketFilter(eventIds = 30073969))
+    
+    PL <- 
+        my_bf$marketCatalogue(maxResults = 1000, filter = marketFilter(eventIds = 2022802))
+    
+    
+    lapply(LIVWHU,get_and_save_a_market, data_folder)
+    lapply(PL,get_and_save_a_market, data_folder)
+    
+    print(Sys.time())
+    Sys.sleep(time = 150)
+}
+
+###############################
+
+# my_bf$events(filter = marketFilter(competitionIds = 10932509)) %>% View() # 30073930	
+
+
+while (Sys.time() < "2020-11-01 13:50:00 BST"){
+    
+    AVLSOU <- 
+        my_bf$marketCatalogue(maxResults = 100, filter = marketFilter(eventIds = 30073930))
+    
+    PL <- 
+        my_bf$marketCatalogue(maxResults = 1000, filter = marketFilter(eventIds = 2022802))
+    
+    
+    lapply(AVLSOU,get_and_save_a_market, data_folder)
+    lapply(PL,get_and_save_a_market, data_folder)
+    
+    print(Sys.time())
+    Sys.sleep(time = 200)
+}
+
+###############################
+
+# my_bf$events(filter = marketFilter(competitionIds = 10932509)) %>% View() # 30072810	
+
+
+while (Sys.time() > "2020-11-01 13:50:00 BST" & Sys.time() < "2020-11-01 15:50:00 BST"){
+    
+    NEWEVE <- 
+        my_bf$marketCatalogue(maxResults = 100, filter = marketFilter(eventIds = 30072810))
+    
+    PL <- 
+        my_bf$marketCatalogue(maxResults = 1000, filter = marketFilter(eventIds = 2022802))
+    
+    
+    lapply(NEWEVE,get_and_save_a_market, data_folder)
+    lapply(PL,get_and_save_a_market, data_folder)
+    
+    print(Sys.time())
+    Sys.sleep(time = 200)
+}
+
+###############################
+
+# my_bf$events(filter = marketFilter(competitionIds = 10932509)) %>% View() # 30072794	
+
+
+while (Sys.time() < "2020-11-01 18:20:00 BST"){
+    
+    MUNARS <- 
+        my_bf$marketCatalogue(maxResults = 100, filter = marketFilter(eventIds = 30072794))
+    
+    PL <- 
+        my_bf$marketCatalogue(maxResults = 1000, filter = marketFilter(eventIds = 2022802))
+    
+    
+    lapply(MUNARS,get_and_save_a_market, data_folder)
+    lapply(PL,get_and_save_a_market, data_folder)
+    
+    print(Sys.time())
+    Sys.sleep(time = 80)
+}
+
+###############################
+
+# my_bf$events(filter = marketFilter(competitionIds = 10932509)) %>% View() # 30072795	
+
+
+while (Sys.time() < "2020-11-01 21:10:00 BST"){
+    
+    TOTBRI <- 
+        my_bf$marketCatalogue(maxResults = 100, filter = marketFilter(eventIds = 30072795))
+    
+    PL <- 
+        my_bf$marketCatalogue(maxResults = 1000, filter = marketFilter(eventIds = 2022802))
+    
+    
+    lapply(TOTBRI,get_and_save_a_market, data_folder)
+    lapply(PL,get_and_save_a_market, data_folder)
+    
+    print(Sys.time())
+    Sys.sleep(time = 70)
+}
+
+###############################
+
+Sys.sleep(time = 200)
